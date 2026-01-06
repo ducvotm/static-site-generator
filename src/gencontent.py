c9148d7f7764a737a54430bc3026398a3885cc9f
ducvotm <vtminhduc140101@gmail.com>
2026-01-06 12:09:11 +0700

    Refactor to docs dir

--- src/gencontent.py
+++ src/gencontent.py
@@ -9,7 +9,7 @@ def generate_pages_recursive(dir_path_content, template_path, dest_dir_path, bas
         from_path = os.path.join(dir_path_content, file_name)
         dest_path = os.path.join(dest_dir_path, file_name)
         if os.path.isfile(from_path):
-            dest_path = Path(dest_path).withsuffix(".html")
+            dest_path = Path(dest_path).with_suffix(".html")
             generate_page(from_path, template_path, dest_path, basepath)
         else:
                 generate_pages_recursive(from_path, template_path, dest_path, basepath)
--- src/main.py
+++ src/main.py
@@ -5,7 +5,7 @@ from copystatic import copy_files_recursive
 from gencontent import generate_page, generate_pages_recursive
 
 dir_path_static = "./static"
-dir_path_public = "./public"
+dir_path_docs = "./docs"
 
 dir_path_content = "./content"
 template_path = "./template.html"
@@ -15,18 +15,18 @@ def main():
     if len(sys.argv) >= 2:
         basepath = sys.argv[1]
 
-    print("Deleting public directory...")
-    if os.path.exists(dir_path_public):
-        shutil.rmtree(dir_path_public)
+    print("Deleting docs directory...")
+    if os.path.exists(dir_path_docs):
+        shutil.rmtree(dir_path_docs)
 
     print("Copying static files to public directory...")
-    copy_files_recursive(dir_path_static, dir_path_public)
+    copy_files_recursive(dir_path_static, dir_path_docs)
     
     print("Generating page recursively ...")
     generate_pages_recursive(
         dir_path_content,
         template_path,
-        dir_path_public,
+        dir_path_docs,
         basepath,
     )
 
